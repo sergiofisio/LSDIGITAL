56891e889ddfbdfbafcf12a3354a68ccd3a61949
sergiobastosfisio@gmail.com <sergiobastosfisio@gmail.com>
2024-04-22 21:52:03 -0300

    banner

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,16 @@
 import logo from "../../assets/logo.svg";
+import Banner from "../banner";
 import Nav from "../nav";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-center h-full w-full p-10 bg-[#000C2F]">
+    <header className="flex items-end justify-center h-full w-full p-10 bg-[#000C2F] relative">
       <div className="flex flex-col items-center justify-center h-full w-1/5 text-white">
         <img className="w-24" src={logo} alt="Logo" />
         <h1 className="text-4xl font-madimi">SL Digital</h1>
+      </div>
+      <div className="absolute top-0 h-[60%] w-1/3">
+        <Banner />
       </div>
       <Nav />
     </header>
